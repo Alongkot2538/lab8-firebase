89965cf4329d8c664ebec54d95dcf8c56f30ebfa
alongkot2538 <alongkot628@gmail.com>
2020-03-19 12:51:47 +0700

    first

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyA2ScCGRIsRJDda3T-s8gfDqUO2RjBESbw",
+  authDomain: "todo242311.firebaseapp.com",
+  databaseURL: "https://todo242311.firebaseio.com",
+  projectId: "todo242311",
+  storageBucket: "todo242311.appspot.com",
+  messagingSenderId: "848179918414",
+  appId: "1:848179918414:web:8d32c2fb4615360f56b51f",
+  measurementId: "G-T2RHBTF8NV"
+  };
+  export default config
